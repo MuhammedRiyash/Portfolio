1547a1cd2e33259137ae08d7bb287f253d7e92c4
Muhammad Riyash <thismd.riyash@gmail.com>
2026-01-28 21:56:13 +0530

    feat: Implement React Router with lazy loading and simplify Vite base path configuration.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: env.VITE_REPO_NAME ? `/${env.VITE_REPO_NAME}/` : "/",
+    base: "/",
     plugins: [react()],
     server: {
       open: true,
